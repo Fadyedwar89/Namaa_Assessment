fed009e072c5ff7c91c9eeefc18797d34f3ac0fe
Fady Edwar Gad <edwarfadyms98@gmail.com>
2026-07-05 03:03:40 +0300

    Edit NotificationHub

--- BackEnd/NamaaCommerce/Services/Implementation/NotificationService/NotificationHub.cs
+++ BackEnd/NamaaCommerce/Services/Implementation/NotificationService/NotificationHub.cs
@@ -7,29 +7,29 @@ namespace Services.Implementation.NotificationService
     [Authorize]
     public class NotificationHub : Hub
     {
-        public class NotificationHub : Hub
+    
+
+    public override async Task OnConnectedAsync()
+    {
+        Console.WriteLine("========== HUB ==========");
+        Console.WriteLine($"Authenticated: {Context.User?.Identity?.IsAuthenticated}");
+        Console.WriteLine($"Name: {Context.User?.Identity?.Name}");
+        Console.WriteLine($"IsAdmin: {Context.User?.IsInRole("Admin")}");
+
+        foreach (var claim in Context.User.Claims)
         {
-            public override async Task OnConnectedAsync()
-            {
-                Console.WriteLine("========== HUB ==========");
-                Console.WriteLine($"Authenticated: {Context.User?.Identity?.IsAuthenticated}");
-                Console.WriteLine($"Name: {Context.User?.Identity?.Name}");
-                Console.WriteLine($"IsAdmin: {Context.User?.IsInRole("Admin")}");
-
-                foreach (var claim in Context.User.Claims)
-                {
-                    Console.WriteLine($"{claim.Type} = {claim.Value}");
-                }
-
-                if (Context.User.IsInRole("Admin"))
-                {
-                    Console.WriteLine("Added To Admin Group");
-                    await Groups.AddToGroupAsync(Context.ConnectionId, "Admins");
-                }
-
-                await base.OnConnectedAsync();
-            }
+            Console.WriteLine($"{claim.Type} = {claim.Value}");
         }
+
+        if (Context.User.IsInRole("Admin"))
+        {
+            Console.WriteLine("Added To Admin Group");
+            await Groups.AddToGroupAsync(Context.ConnectionId, "Admins");
+        }
+
+        await base.OnConnectedAsync();
+    }
+
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
             if (Context.User.IsInRole("Admin"))
